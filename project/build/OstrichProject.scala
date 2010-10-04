91c6267b58567674c36799eb1d4371e2f4033956
Yung-Luen Lan <yllan@me.com>
2010-10-04 19:23:07 +0800

    Add self-compiled library json and xrayspecs.

--- project/build/OstrichProject.scala
+++ project/build/OstrichProject.scala
@@ -2,12 +2,12 @@ import sbt._
 
 import com.twitter.sbt._
 
-class OstrichProject(info: ProjectInfo) extends StandardProject(info) with SubversionPublisher {
+class OstrichProject(info: ProjectInfo) extends DefaultProject(info) {
   val specs = "org.scala-tools.testing" % "specs_2.8.0" % "1.6.5"
   val testinterface = "org.scala-tools.testing" % "test-interface" % "0.5" 
   
   val vscaladoc = "org.scala-tools" % "vscaladoc" % "1.1-md-3"
-  val twitterJson = "com.twitter" % "json" % "2.1.3"
+  // val twitterJson = "com.twitter" % "json" % "2.1.3"
   val configgy = "net.lag" % "configgy" % "2.0.0"
   val commonsLogging = "commons-logging" % "commons-logging" % "1.1"
   val commonsLang = "commons-lang" % "commons-lang" % "2.2"
@@ -21,14 +21,4 @@ class OstrichProject(info: ProjectInfo) extends StandardProject(info) with Subve
 
   val jbossRepo = "JBoss Repository" at "http://repository.jboss.org/nexus/content/groups/public/"
 
-  override def pomExtra =
-    <licenses>
-      <license>
-        <name>Apache 2</name>
-        <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
-        <distribution>repo</distribution>
-      </license>
-    </licenses>
-
-  // override def subversionRepository = Some("http://svn.local.twitter.com/maven-public")
 }
